6bcd0af94de3db0062742eeb43c4f2e6fc49eb1a
Djohan Effendy <djohan34@gmail.com>
2020-08-14 00:59:17 +0700

    First Revision Last Day

--- src/app/content/channel/channel.component.ts
+++ src/app/content/channel/channel.component.ts
@@ -32,11 +32,15 @@ export class ChannelComponent implements OnInit {
     private apollo: Apollo
   ) {}
 
+  lastKey: number = 0;
+  observer: any;
+
   ngOnInit(): void {
     this.currentUserParam = this.route.snapshot.params.id;
     this.filterTop5Videos();
     this.filterGetVideos();
     this.filterRecentVideos();
+    this.sortVideosByViews();
     this.apollo
       .watchQuery<any>({
         query: searchUserById,
@@ -47,12 +51,30 @@ export class ChannelComponent implements OnInit {
       .valueChanges.subscribe((result) => {
         this.userFromPickedChannel = result.data.searchUserByID;
       });
+
+    this.lastKey = 4;
+    this.observer = new IntersectionObserver((entry) => {
+      if (entry[0].isIntersecting) {
+        let main = document.querySelector('.parent');
+        for (let i = 0; i < 3; i++) {
+          if (this.lastKey < this.filteredCurrentVideos.length) {
+            let div = document.createElement('div');
+            let video = document.createElement('app-video-renderer');
+            video.setAttribute('video', 'this.videos[this.lastKey]');
+            div.appendChild(video);
+            main.appendChild(div);
+            this.lastKey++;
+          }
+        }
+      }
+    });
+    this.observer.observe(document.querySelector('.end-point'));
   }
 
   userFromPickedChannel: any = null;
 
-  homeState: boolean = true;
-  videosState: boolean = false;
+  homeState: boolean = false;
+  videosState: boolean = true;
   playlistState: boolean = false;
   communityState: boolean = false;
   aboutState: boolean = false;
@@ -170,5 +192,66 @@ export class ChannelComponent implements OnInit {
         break;
       }
     }
+  }
+
+  // sort
+
+  mostPopularState: boolean = true;
+  newestState: boolean = false;
+  oldestState: boolean = false;
+
+  changeMostPopularState() {
+    this.mostPopularState = true;
+    this.newestState = false;
+    this.oldestState = false;
+    this.sortVideosByViews();
+  }
+
+  changeNewestState() {
+    this.mostPopularState = false;
+    this.newestState = true;
+    this.oldestState = false;
+    this.sortVideosByNewest();
+  }
+
+  changeOldestState() {
+    this.mostPopularState = false;
+    this.newestState = false;
+    this.oldestState = true;
+    this.sortVideosByOldest();
+  }
+
+  sortVideosByNewest() {
+    this.filteredCurrentVideos = [] = this.filteredCurrentVideos.sort(
+      (n1, n2) => {
+        var first = new Date(n1.upload_date);
+        var second = new Date(n2.upload_date);
+        if (first.getTime() < second.getTime()) {
+          return 1;
+        } else return -1;
+      }
+    );
+  }
+
+  sortVideosByOldest() {
+    this.filteredCurrentVideos = [] = this.filteredCurrentVideos.sort(
+      (n1, n2) => {
+        var first = new Date(n1.upload_date);
+        var second = new Date(n2.upload_date);
+        if (first.getTime() > second.getTime()) {
+          return 1;
+        } else return -1;
+      }
+    );
+  }
+
+  sortVideosByViews() {
+    this.filteredCurrentVideos = [] = this.filteredCurrentVideos.sort(
+      (n1, n2) => {
+        if (n1.view_count < n2.view_count) {
+          return 1;
+        } else return -1;
+      }
+    );
   }
 }
